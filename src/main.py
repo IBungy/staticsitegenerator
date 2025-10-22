4ceb16b446fea1d8ae7695868815f39706091bde
IBungy <bcain4902@gmail.com>
2025-10-22 15:09:17 -0400

    fixed bug

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import TextNode, TextType
 
 def main():
-    node = TextNode("hello", BOLD_TEXT, peener.com)
+    node = TextNode("hello", TextType.BOLD_TEXT, peener.com)
     print(node)
 
 
