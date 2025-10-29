8f67999af392c8230933f6c4889989a91da8ff5d
IBungy <bcain4902@gmail.com>
2025-10-29 11:36:53 -0400

    added tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,6 +8,14 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
+    def test_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a different text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+    def test_text_types(self):
+        node_plain = TextNode("Plain text", TextType.PLAIN)
+        node_bold = TextNode("Plain text", TextType.BOLD)
+        self.assertNotEqual(node_plain, node_bold)
 
 
 if __name__ == "__main__":
