5e3a3dd5db640be5da316d8898bbdc55e4fa9ddf
IBungy <bcain4902@gmail.com>
2025-11-04 11:35:11 -0500

    fixed child class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -26,14 +26,16 @@ class HTMLNode:
             self.props == other.props
         )
     
-    class LeafNode(HTMLNode):
-        def __init__(self, tag, value, props=None):
-            super().__init__(tag=tag, value=value, children=None, props=props)
-            
-        def to_html(self):
-            if not self.value:
-                raise ValueError() 
-            if not self.tag:
-                return self.value
-            attrs = self.props_to_html()
-            return f"<{self.tag}{attrs}>{self.value}</{self.tag}>"
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
